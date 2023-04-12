9007255c0d0457e5b03dbc3f0391fd90c9e6c03c
huuluc10 <lucnguyenhuu91@gmail.com>
2023-04-12 22:38:30 +0700

    Cung cấp AuthRepository cho lớp AuthController

--- features/auth/repository/auth_repository.dart
+++ features/auth/repository/auth_repository.dart
@@ -3,6 +3,14 @@ import 'package:chatapp_clone_whatsapp/features/auth/screens/otp_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    auth: FirebaseAuth.instance,
+    firestore: FirebaseFirestore.instance,
+  ),
+);
 
 class AuthRepository {
   final FirebaseAuth auth;
@@ -10,17 +18,24 @@ class AuthRepository {
 
   AuthRepository({required this.auth, required this.firestore});
 
-  void  singInWithPhone(BuildContext context ,String phoneNumber) async {
+  void singInWithPhone(BuildContext context, String phoneNumber) async {
     try {
-      await auth.verifyPhoneNumber(phoneNumber: phoneNumber, verificationCompleted: (PhoneAuthCredential credential) async {
-        await auth.signInWithCredential(credential);
-      }, verificationFailed: (e) {
-        throw Exception(e.message);
-      }, codeSent: ((String verificationId, int? resendToken) async {
-        Navigator.push(context, OTPScreen.routeName, argumemnts: verificationId,);
-      }), codeAutoRetrievalTimeout: (String verificationId) {
-
-      });
+      await auth.verifyPhoneNumber(
+          phoneNumber: phoneNumber,
+          verificationCompleted: (PhoneAuthCredential credential) async {
+            await auth.signInWithCredential(credential);
+          },
+          verificationFailed: (e) {
+            throw Exception(e.message);
+          },
+          codeSent: ((String verificationId, int? resendToken) async {
+            Navigator.push(
+              context,
+              OTPScreen.routeName,
+              argumemnts: verificationId,
+            );
+          }),
+          codeAutoRetrievalTimeout: (String verificationId) {});
     } on FirebaseAuthException catch (e) {
       showSnackBar(context: context, content: e.message.toString());
     }
