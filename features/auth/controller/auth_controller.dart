2d9f0ebbeca1693b4c7ef0ebb096c21877748bdb
huuluc10 <lucnguyenhuu91@gmail.com>
2023-04-12 22:43:27 +0700

    Xây dựng lớp AuthController

---
+++ features/auth/controller/auth_controller.dart
@@ -0,0 +1,18 @@
+import 'package:chatapp_clone_whatsapp/features/auth/repository/auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+final authControllerProvider = Provider((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AuthController(authRepository: authRepository);
+});
+
+class AuthController {
+  final AuthRepository authRepository;
+
+  AuthController({required this.authRepository});
+
+  void signInWithPhone(BuildContext context, String phoneNumber ) {
+    authRepository.singInWithPhone(context, phoneNumber);
+  }
+}
