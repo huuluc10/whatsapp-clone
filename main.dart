e459f4bd393f423070692498e6f14dcbae2267ca
huuluc10 <lucnguyenhuu91@gmail.com>
2023-04-12 16:56:21 +0700

    Tạo sha1 key và lưu trong main.dart

--- main.dart
+++ main.dart
@@ -2,6 +2,36 @@ import 'package:chatapp_clone_whatsapp/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+// Variant: debugAndroidTest
+// Config: debug
+// Store: C:\Users\lucng\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 30:57:10:F5:44:D6:4D:04:58:98:9A:F1:0F:BD:C0:96
+// SHA1: BE:23:72:19:88:CE:B2:32:1A:EB:1D:49:D7:C7:ED:2D:52:99:FE:3D
+// SHA-256: 38:11:7F:0B:0B:F9:1C:6C:BB:6F:C5:FE:E3:57:61:CD:9D:E0:5A:00:CB:C6:6A:8D:11:46:92:EB:A5:06:7E:8B
+// Valid until: Sunday, January 5, 2053
+// ----------
+// > Task :app:signingReport
+// Variant: debug
+// Config: debug
+// Store: C:\Users\lucng\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 30:57:10:F5:44:D6:4D:04:58:98:9A:F1:0F:BD:C0:96
+// SHA1: BE:23:72:19:88:CE:B2:32:1A:EB:1D:49:D7:C7:ED:2D:52:99:FE:3D
+// SHA-256: 38:11:7F:0B:0B:F9:1C:6C:BB:6F:C5:FE:E3:57:61:CD:9D:E0:5A:00:CB:C6:6A:8D:11:46:92:EB:A5:06:7E:8B
+// Valid until: Sunday, January 5, 2053
+// ----------
+// Variant: release
+// Config: debug
+// Store: C:\Users\lucng\.android\debug.keystore
+// Alias: AndroidDebugKey
+// MD5: 30:57:10:F5:44:D6:4D:04:58:98:9A:F1:0F:BD:C0:96
+// SHA1: BE:23:72:19:88:CE:B2:32:1A:EB:1D:49:D7:C7:ED:2D:52:99:FE:3D
+// SHA-256: 38:11:7F:0B:0B:F9:1C:6C:BB:6F:C5:FE:E3:57:61:CD:9D:E0:5A:00:CB:C6:6A:8D:11:46:92:EB:A5:06:7E:8B
+// Valid until: Sunday, January 5, 2053
+// ----------
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -19,15 +49,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -37,16 +58,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
