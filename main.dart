1bbced84a517268729a3c11265ba9914148d4d3f
huuluc10 <lucnguyenhuu91@gmail.com>
2023-04-12 11:55:06 +0700

    Cài đặt firebase

--- main.dart
+++ main.dart
@@ -1,6 +1,12 @@
+import 'package:chatapp_clone_whatsapp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
