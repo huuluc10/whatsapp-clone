b2a062c9a8ee4b2c512d3e2132d7f05c8d88fe4e
Nguyễn Hữu Lực <lucnguyenhuu91@gmail.com>
2023-05-11 19:52:38 +0700

    Chỉnh sửa màn hình list

--- common/widgets/contacts_list.dart
+++ common/widgets/contacts_list.dart
@@ -8,59 +8,53 @@ class ContactsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder<List<ChatContact>>(
-                // stream: ,
-                builder: (context, snapshot) {
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: info.length,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: ListTile(
-                        title: Text(
-                          info[index]['name'].toString(),
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                        subtitle: Padding(
-                          padding: const EdgeInsets.only(top: 6),
-                          child: Text(
-                            info[index]['message'].toString(),
-                            style: const TextStyle(fontSize: 15),
+    return StreamBuilder<Object>(
+        stream: null,
+        builder: (context, snapshot) {
+          return Column(
+            children: [
+              Expanded(
+                child: StreamBuilder<List<ChatContact>>(
+                    // stream: ,
+                    builder: (context, snapshot) {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: info.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {},
+                        child: ListTile(
+                          title: Text(
+                            info[index]['name'].toString(),
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.only(top: 6),
+                            child: Text(
+                              info[index]['message'].toString(),
+                              style: const TextStyle(fontSize: 15),
+                            ),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              info[index]['profilePic'].toString(),
+                            ),
+                          ),
+                          trailing: Text(
+                            info[index]['time'].toString(),
+                            style: const TextStyle(
+                              fontSize: 13,
+                              color: Colors.grey,
+                            ),
                           ),
                         ),
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            info[index]['profilePic'].toString(),
-                          ),
-                        ),
-                        trailing: Text(
-                          info[index]['time'].toString(),
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                    ),
+                      );
+                    },
                   );
-                },
-              );
-            }),
-          ),
-          const Divider(
-            color: dividerColor,
-            indent: 85,
-          ),
-        ],
-      ),
-    );
+                }),
+              ),
+            ],
+          );
+        });
   }
 }
